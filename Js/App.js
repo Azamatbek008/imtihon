b34475b683d9f93d2ee40e8bd2c49c3d90165661
Azamatbek008 <azamatbek0834@gmail.com>
2024-02-28 11:32:49 +0300

    Initial commit

---
+++ Js/App.js
@@ -0,0 +1,72 @@
+// function foo() {
+//     const body - document.body,
+//     sun - document.querySelector('#sun'),
+//     moon - document.querySelector('#moon'),
+//     logoImgDark -document.querySelector('.logo-img-dark'),
+//     logoImg - document.querySelector('.logo img')
+
+
+//     if(body.classList == 'light'){
+//      sun.classList.remove('hight')
+//      sun.classList.add('show')
+//      moon.classList.remove('show')
+//      moon.classList.add('hight')
+//      body.classList.remove('light')
+//      body.classList.add('dark')
+//      logoImgDark.classList.remove('show')
+//      logoImgDark.classList.add('hight')
+//      logoImg .classList.add('show')
+//      logoImg .classList.remove('hight')
+//     }else{
+//         sun.classList.remove('show')
+//         sun.classList.add('hight')
+//         moon.classList.remove('hight')
+//         moon.classList.add('show')
+//         body.classList.remove('dark')
+//         body.classList.add('light')
+//         logoImgDark.classList.remove('hight')
+//         logoImgDark.classList.add('show')
+//         logoImg .classList.add('show')
+//         logoImg .classList.remove('show')
+//     }
+// }
+
+// function menuClick(){
+//     const open - document.querySelector('option'),
+//     close - document.querySelector('#close'),
+//     menu - document.querySelector('#menu');
+
+//     if(open.classList == 'hight'){
+//         open.classList .remove('hight')
+//         open.classList.add('show')
+//         close.classList.remove('show')
+//         close.classList.add('hight')
+//         menu.classList.remove('active')
+//     }else{
+//         open.classList .remove('show')
+//         open.classList.add('hight')
+//         close.classList.remove('hight')
+//         close.classList.add('show')
+//         menu.classList.add('active')
+//     }
+// }
+
+/*toggle 'dark-mode' class  */
+const themeSwitch = document.getElementById('input-theme-switch');
+
+themeSwitch.addEventListener('click', () => {
+  if(themeSwitch.checked) {
+    darkModeOn();
+  }
+  else{
+    darkModeOff();
+  }
+});
+
+function darkModeOn(){
+  document.body.classList.add('dark-mode');
+}
+
+function darkModeOff(){
+  document.body.classList.remove('dark-mode');
+}
